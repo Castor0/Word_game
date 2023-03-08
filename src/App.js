209f83e179fc4c17f0717280cc7df740671265f7
Castor <gregsmarv@gmail.com>
2023-03-08 13:24:43 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import axios from 'axios';
 
-function App() {
+const App = () => {
+
+
+  const options = {
+    method: 'GET',
+    url: 'https://twinword-word-association-quiz.p.rapidapi.com/type1/',
+    params: {level: '3', area: 'sat'},
+    headers: {
+      'X-RapidAPI-Key': 'd33bcfb4b7mshfa35f5acb22b2b3p14b3f0jsn4333c2331e4b',
+      'X-RapidAPI-Host': 'twinword-word-association-quiz.p.rapidapi.com'
+    }
+  };
+  
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
+
+
   return (
     <div className="App">
       
